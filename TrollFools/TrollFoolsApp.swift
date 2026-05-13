980bb9c0925f6b659fbb49387d941dbcd358a72d
wkk <chenqing19950908@gmail.com>
2026-05-13 12:29:58 +0800

    更新 TrollFoolsApp.swift

--- TrollFools/TrollFoolsApp.swift
+++ TrollFools/TrollFoolsApp.swift
@@ -10,26 +10,14 @@ import SwiftUI
 @main
 struct TrollFoolsApp: SwiftUI.App {
 
-    @AppStorage("isDisclaimerHiddenV2")
-    var isDisclaimerHidden: Bool = false
-
     init() {
         try? FileManager.default.removeItem(at: InjectorV3.temporaryRoot)
     }
 
     var body: some Scene {
         WindowGroup {
-            ZStack {
-                if isDisclaimerHidden {
-                    AppListView()
-                        .environmentObject(AppListModel())
-                        .transition(.opacity)
-                } else {
-                    DisclaimerView(isDisclaimerHidden: $isDisclaimerHidden)
-                        .transition(.opacity)
-                }
-            }
-            .animation(.easeInOut, value: isDisclaimerHidden)
+            AppListView()
+                .environmentObject(AppListModel())
         }
     }
 }
